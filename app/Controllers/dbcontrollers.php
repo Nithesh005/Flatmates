2538186664e147a58ababd3868616cd491715b37
Nitheshwaran <nitheshwaran003@gmail.com>
2023-05-02 00:08:56 +0530

    db

--- app/Controllers/dbcontrollers.php
+++ app/Controllers/dbcontrollers.php
@@ -17,7 +17,8 @@ class dbcontrollers extends BaseController{
     public function reg_user_data(){
         $sname = $this->request->getvar('tenant_mail');
         // $tenant_pasword = $this->request->getvar('tenant_pasword');
-        $res = $this->datas->reg_user_data_model();   
+        $reg_data['sname'] = $this->request->getvar('tenant_mail');
+        $res = $this->datas->reg_user_data_model($reg_data);   
         echo json_encode("res");
     }
 }
--- app/Models/dbmodels.php
+++ app/Models/dbmodels.php
@@ -6,8 +6,17 @@ use CodeIgniter\CLI\Console;
 use CodeIgniter\Model;
 
 class Intern_Model extends Model{
-    public function reg_user_data_model(){
-        return "hai from model";
+    public function reg_user_data_model($reg_data){
+        $db = \Config\Database::connect();
+
+        $my_demo_data = [
+            "db_name" => $reg_data['intern_id'],
+            
+        ];
+
+        $query = $db->table('tablename');
+        $res = $query->insert($my_demo_data);
+        return $res;
     }
 }
 
