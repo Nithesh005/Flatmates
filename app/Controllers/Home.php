254df7737736113f61602a58108c7a1523a823fb
Mathan <116017480+MATHAN4023@users.noreply.github.com>
2023-05-01 11:39:49 +0530

    home css file

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
     public function index()
     {
     //     return view('tenat_dashboard');
-    return view('owner_dashbord');
+    return view('home');
 
     }
     // public function index()
