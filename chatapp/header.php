d029f6755304be7d3f906366156dd65c476104d5
Nitheshwaran <nitheshwaran003@gmail.com>
2023-05-09 19:01:17 +0530

    tenant dashboard

--- chatapp/header.php
+++ chatapp/header.php
@@ -7,5 +7,5 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Realtime Chat App | CodingNepal</title>
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/> -->
 </head>
--- chatapp/php/search.php
+++ chatapp/php/search.php
@@ -2,10 +2,11 @@
     session_start();
     include_once "config.php";
 
-    $outgoing_id = $_SESSION['unique_id'];
+    // $outgoing_id = $_SESSION['unique_id'];
+    $outgoing_id = 'FMTN_1006';
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
 
-    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
+    $sql = "SELECT * FROM tenant_reg WHERE NOT u_id = 'FMTN_1006' AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
     $output = "";
     $query = mysqli_query($conn, $sql);
     if(mysqli_num_rows($query) > 0){
