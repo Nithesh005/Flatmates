ca4e15b60d18ce9234c19051954e395b1cb0acfe
Mathan <116017480+MATHAN4023@users.noreply.github.com>
2023-05-12 12:19:04 +0530

    card design

--- app/Views/owner_dashbord.php
+++ app/Views/owner_dashbord.php
@@ -9,8 +9,11 @@ $session = \Config\Services::session();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--bootstrap cdn -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
     <!-- Font aswesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- css -->
@@ -39,6 +42,40 @@ $session = \Config\Services::session();
         /* .row>* {
             margin-left: 8%;
         } */
+        .action-buttons {
+            display: flex;
+            justify-content: flex-end;
+        }
+
+        .accept-button,
+        .reject-button {
+            padding: 10px 20px;
+            margin-left: 10px;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .accept-button {
+            background-color: #4CAF50;
+            color: #fff;
+        }
+
+        .reject-button {
+            background-color: #F44336;
+            color: #fff;
+        }
+
+        .accepted {
+            height: 50px;
+        }
+
+        .user-photo {
+            border-radius: 50%;
+            width: 50px;
+            height: 50px;
+        }
     </style>
 </head>
 
@@ -52,7 +89,8 @@ $session = \Config\Services::session();
             Add New House</button>
     </div>
 
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -62,7 +100,9 @@ $session = \Config\Services::session();
 
 
                 <div class="modal-body">
-                    <form class="row g-3 p-4" action="<?php echo base_url('public/index.php/dbcontrollers/demo_reg'); ?>" method="post" enctype="multipart/form-data">
+                    <form class="row g-3 p-4"
+                        action="<?php echo base_url('public/index.php/dbcontrollers/demo_reg'); ?>" method="post"
+                        enctype="multipart/form-data">
                         <div class="col-md-6">
                             <label for="inputEmail4" class="form-label">House number</label>
                             <input type="text" class="form-control" id="house_no" placeholder="20/93" name="house_no">
@@ -70,32 +110,39 @@ $session = \Config\Services::session();
 
                         <div class="col-6">
                             <label for="inputAddress" class="form-label">Address</label>
-                            <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="1234 Main St">
+                            <input type="text" class="form-control" id="inputAddress" name="inputAddress"
+                                placeholder="1234 Main St">
                         </div>
                         <div class="col-12">
                             <label for="inputAddress2" class="form-label">About your house</label>
-                            <input type="text" class="form-control" id="inputAddress2" name="inputAddress2" placeholder="Apartment, studio, or floor">
+                            <input type="text" class="form-control" id="inputAddress2" name="inputAddress2"
+                                placeholder="Apartment, studio, or floor">
                         </div>
                         <div class="col-12">
                             <label for="Description" class="form-label">Description</label>
-                            <textarea class="form-control" id="Description" name="Description" placeholder="Nearby facilities"></textarea>
+                            <textarea class="form-control" id="Description" name="Description"
+                                placeholder="Nearby facilities"></textarea>
                         </div>
                         <div class="col-md-6">
                             <label for="Members_allowed" class="form-label">Members allowed</label>
-                            <input type="text" class="form-control" id="Members_allowed" name="Members_allowed" placeholder="5 members">
+                            <input type="text" class="form-control" id="Members_allowed" name="Members_allowed"
+                                placeholder="5 members">
                         </div>
                         <div class="col-md-6">
                             <label for="Rent_amount" class="form-label">Rent Amount</label>
-                            <input type="text" class="form-control" id="Rent_amount" name="Rent_amount" placeholder="₹15000-/Per Month">
+                            <input type="text" class="form-control" id="Rent_amount" name="Rent_amount"
+                                placeholder="₹15000-/Per Month">
                         </div>
                         <div class="col-md-6">
                             <label for="inputCity" class="form-label">City</label>
-                            <input type="text" class="form-control" id="inputCity" name="inputCity" placeholder="Madurai">
+                            <input type="text" class="form-control" id="inputCity" name="inputCity"
+                                placeholder="Madurai">
                         </div>
 
                         <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">State</label>
-                            <input type="text" class="form-control" id="inputstate" name="inputstate" placeholder="Tamilnadu">
+                            <input type="text" class="form-control" id="inputstate" name="inputstate"
+                                placeholder="Tamilnadu">
                         </div>
 
                         <div class="col-md-6">
@@ -130,7 +177,8 @@ $session = \Config\Services::session();
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -139,13 +187,21 @@ $session = \Config\Services::session();
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    Not Yet
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                <!-- <div class="modal-body"> -->
+                    <!-- <div class="card accepted"> -->
+                        <div class="card-content">
+                            <div class="user-info">
+                                <img src="https://i.imgur.com/cMSVQZC.jpg" alt="User Photo" class="user-photo">
+                                <p class="username">John Doe</p>
+                                <button class="accept-button">Accept</button>
+                                <button class="reject-button">Reject</button>
+                            </div>
+                                <!-- <div class="action-buttons">
+                                
+                                </div> -->
+                        </div>
+                    <!-- </div> -->
+                <!-- </div> -->
             </div>
         </div>
     </div>
@@ -186,16 +242,16 @@ $session = \Config\Services::session();
         button = document.querySelector(".input-file-trigger"),
         the_return = document.querySelector(".file-return");
 
-    button.addEventListener("keydown", function(event) {
+    button.addEventListener("keydown", function (event) {
         if (event.keyCode == 13 || event.keyCode == 32) {
             fileInput.focus();
         }
     });
-    button.addEventListener("click", function(event) {
+    button.addEventListener("click", function (event) {
         fileInput.focus();
         return false;
     });
-    fileInput.addEventListener("change", function(event) {
+    fileInput.addEventListener("change", function (event) {
         var files = this.files;
         var filenames = "";
         for (var i = 0; i < files.length; i++) {
@@ -210,84 +266,84 @@ $session = \Config\Services::session();
 
     // card design
     // function getcard() {
-        $.ajax({
-            // url: "<?php echo base_url() ?>/public/index.php/Controllers/dbcontrollers/reg_user_data",
-            url: "<?php echo base_url('public/index.php/Dbcontrollers/owner_card'); ?>",
-            method: "POST",
-            dataType: "json",
-            success: function(res) {
-                console.log(res);
-                console.log("ajax woking");
-                $('.owner_card').empty();
-                res.forEach(
-                    function(items) {
-                        var element = $();
-                        element = element.add(
-                            '<div class="card col-3 card_index">' +
-                            '<div class="card-image">' +
-                            // '<span class="card-notify-badge">Low KMS</span>' +
-                            // '<span class="card-notify-year">2018</span>' +
-                            // items.u_id+items.image+
-                            '<img class="img-fluid" ' +
-                            'src="<?php echo base_url() ?>/public/public/uploads/' + items.u_id + '/' + items.image + '" ' +
-                            'alt="Alternate Text" height="100px" width="400px"  />' +
-                            '</div>' +
-                            '<div class="card-image-overlay m-auto">' +
-                            '<span class="card-detail-badge">' + '₹' + items.rent + '</span>' +
-                            '<span class="card-detail-badge">' + items.BHK + '/' + 'BHK' + '</span>' +
-                            '</div>' +
-                            '<div class="col-12">' +
-                            '<div class="card-body">' +
-                            '<h5 class="card-title">About Home</h5>' +
-                            '<div class="card-title house_no" id="'+items.house_no+'" value="mm">'+items.house_no+'</div>' +
-                            '<p class="card-text">' + items.description + '</p>' +
-                            '<p class="card-text">' + 'City' + ':' + items.city + '</p>' +
-                            '<p class="card-text">' + 'Zipcode' + ':' + items.zipcode + '</p>' +
-                            '<br>' +
-                            '<button type="button" id="delete_btn" class="btn btn-danger">Delete Home</button>' +
-                            // '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>'+
-                            '</div>' +
-                            '</div>' + '</div>'
+    $.ajax({
+        // url: "<?php echo base_url() ?>/public/index.php/Controllers/dbcontrollers/reg_user_data",
+        url: "<?php echo base_url('public/index.php/Dbcontrollers/owner_card'); ?>",
+        method: "POST",
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            console.log("ajax woking");
+            $('.owner_card').empty();
+            res.forEach(
+                function (items) {
+                    var element = $();
+                    element = element.add(
+                        '<div class="card col-3 card_index">' +
+                        '<div class="card-image">' +
+                        // '<span class="card-notify-badge">Low KMS</span>' +
+                        // '<span class="card-notify-year">2018</span>' +
+                        // items.u_id+items.image+
+                        '<img class="img-fluid" ' +
+                        'src="<?php echo base_url() ?>/public/public/uploads/' + items.u_id + '/' + items.image + '" ' +
+                        'alt="Alternate Text" height="100px" width="400px"  />' +
+                        '</div>' +
+                        '<div class="card-image-overlay m-auto">' +
+                        '<span class="card-detail-badge">' + '₹' + items.rent + '</span>' +
+                        '<span class="card-detail-badge">' + items.BHK + '/' + 'BHK' + '</span>' +
+                        '</div>' +
+                        '<div class="col-12">' +
+                        '<div class="card-body">' +
+                        '<h5 class="card-title">About Home</h5>' +
+                        '<div class="card-title house_no" id="' + items.house_no + '" value="mm">' + items.house_no + '</div>' +
+                        '<p class="card-text">' + items.description + '</p>' +
+                        '<p class="card-text">' + 'City' + ':' + items.city + '</p>' +
+                        '<p class="card-text">' + 'Zipcode' + ':' + items.zipcode + '</p>' +
+                        '<br>' +
+                        '<button type="button" id="delete_btn" class="btn btn-danger">Delete Home</button>' +
+                        // '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>'+
+                        '</div>' +
+                        '</div>' + '</div>'
 
-                        );
-                        // element.find('.house_no').data('house_no', items.house_no);
-                        $('.owner_card').append(element);
+                    );
+                    // element.find('.house_no').data('house_no', items.house_no);
+                    $('.owner_card').append(element);
 
-                        element.find('#delete_btn').click(function() {
-                            var house_no = items.house_no;
-                            // alert(house_no);
-                            // alert(a);
-                            // Perform deletion logic for the corresponding card
-                            $.ajax({
-                                url: "<?php echo base_url('public/index.php/dbcontrollers/delete_owner_card'); ?>",
-                                method:"POST",
-                                data:{
-                                    house_no : house_no,
-                                },
-                                success:function(res){
-                                    alert(res);
-                                    return location.reload();
-                                },
-                                error:function(er){
-                                    alert("error");
-                                }
-                                
-                            })
-                        });
+                    element.find('#delete_btn').click(function () {
+                        var house_no = items.house_no;
+                        // alert(house_no);
+                        // alert(a);
+                        // Perform deletion logic for the corresponding card
+                        $.ajax({
+                            url: "<?php echo base_url('public/index.php/dbcontrollers/delete_owner_card'); ?>",
+                            method: "POST",
+                            data: {
+                                house_no: house_no,
+                            },
+                            success: function (res) {
+                                alert(res);
+                                return location.reload();
+                            },
+                            error: function (er) {
+                                alert("error");
+                            }
 
-                    }
+                        })
+                    });
 
-                );
-                // $('#delete_btn').click(function() {
-                //     alert("hai");
-                // });
+                }
 
-            },
-            error: function(er) {
-                // console.error(er);
-                console.log("error");
-            }
-        })
+            );
+            // $('#delete_btn').click(function() {
+            //     alert("hai");
+            // });
+
+        },
+        error: function (er) {
+            // console.error(er);
+            console.log("error");
+        }
+    })
 
     // }
     // $('#delete_btn').click(function() {
