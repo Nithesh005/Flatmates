a3d2b04cb2bfe333a073b7b80e7cefbef6780877
Mathan <116017480+MATHAN4023@users.noreply.github.com>
2023-05-15 16:21:45 +0530

    check

--- app/Views/owner_dashbord.php
+++ app/Views/owner_dashbord.php
@@ -180,8 +180,8 @@ $session = \Config\Services::session();
     </button> -->
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-xl" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Requests</h5>
