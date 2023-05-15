877a71d7d0f7d8309497bbb54ecacf24a0e63a36
Mathan <116017480+MATHAN4023@users.noreply.github.com>
2023-05-16 00:04:28 +0530

    codes

--- app/Views/tenat_dashboard.php
+++ app/Views/tenat_dashboard.php
@@ -369,18 +369,14 @@ $session = \Config\Services::session();
                             '</div>' +
                             '<div class="card-image-overlay m-auto">' +
                             '<span class="card-detail-badge">' + '₹' + items.rent + '/' + '</span>' +
-                            '<span class="card-detail-badge">' + items.BHK + '/' + 'BHK' + '</span>' +
+                            '<span class="card-detail-badge">' + items.BHK + '-' + 'BHK' + '</span>' +
                             '</div>' +
                             '<div class="col-12">' +
                             '<div class="card-body">' +
-                            '<h6 class="card-title">House No</h6>' +
-                            '<p class="card-text">' + items.house_no + '</p>' +
-                            '<h6 class="card-title">DESCRIPTION</h6>' +
-                            '<p class="card-text">' + items.about + '</p>' +
-                            '<h6 class="card-title">About Home</h6>' +
-                            '<p class="card-text">' + items.description + '</p>' +
-                            '<h6 class="card-title">Members allowed</h6>' +
-                            '<p class="card-text">' + items.members + '</p>' +
+                            '<p class="card-text">' +'House No'+ ':' + items.house_no + '</p>' +
+                            '<p class="card-text">' + 'DESCRIPTION' + ':' + items.description + '</p>' +
+                            '<p class="card-text">' + 'About Home' + ':' + items.about + '</p>' +
+                            '<p class="card-text">'+'Members allowed'+ ':' + items.members + '</p>' +
                             '<p class="card-text">' + 'City' + ':' + items.city + '</p>' +
                             '<p class="card-text">' + 'Zipcode' + ':' + items.zipcode + '</p>' +
                             '<br>' +
